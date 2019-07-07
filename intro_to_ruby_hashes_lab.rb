7a691a35f1ab25b6e3fe8e1fc4b47aa4da7c79e0
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 03:16:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,13 +16,13 @@ def pioneer
 end
 
 def id_generator
- id generator = {
+ id_generator = {
    :id => 35
  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+ 
 end
 
 def read_from_hash(hash, key)
